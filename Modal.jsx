123d4f7a637afbb502eb7b8805bfc40a649df611
Alireza shahbaziyan <87496598+Alireza-shahbaziyan@users.noreply.github.com>
2025-02-13 16:18:46 +0330

    Update Modal.jsx
    update and add icon

--- Modal.jsx
+++ Modal.jsx
@@ -1,35 +1,46 @@
 import PropTypes from "prop-types";
 
-const Modal = ({ isOpen, onClose, children, title }) => {
+const ModalCmp = ({ isOpen, onClose, children, title,width }) => {
   if (!isOpen) return null;
-
+   // Define allowed width classes
+   const widthClasses = {
+    small: "w-64",    // 16rem
+    medium: "w-96",   // 24rem
+    large: "w-[29rem]",
+    xlarge: "w-[32rem]",
+    full: "w-full",
+  };
+   // Default to medium if width is not recognized
+   const modalWidth = widthClasses[width] || "w-96";
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg p-6 w-96 shadow-lg relative">
+      <div className={`bg-white rounded-lg p-6 shadow-lg relative ${modalWidth}`}>
         {/* Header */}
-        {title && <h2 className="text-lg font-semibold text-center mb-4">{title}</h2>}
+        <div className="flex justify-between w-full">
+          {title && <div className="text-lg font-bold text-center ">{title}</div>}
+          {/* Close Button */}
+          <button
+            onClick={onClose}
+            className="text-gray-500 hover:text-gray-700"
+          >
+              ✖️
+          </button>
+        </div>
 
         {/* Content */}
         <div>{children}</div>
-
-        {/* Close Button */}
-        <button
-          onClick={onClose}
-          className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
-        >
-          ✖
-        </button>
       </div>
     </div>
   );
 };
 
 
-Modal.propTypes = {
-  isOpen: PropTypes.bool.isRequired, 
-  onClose: PropTypes.func.isRequired, 
+ModalCmp.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
-  title: PropTypes.string, 
+  title: PropTypes.string,
+  width:PropTypes.string
 };
 
-export default Modal;
+export default ModalCmp;
